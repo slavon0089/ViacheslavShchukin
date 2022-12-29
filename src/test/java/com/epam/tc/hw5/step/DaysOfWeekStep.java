5b8f65aba9c84a7f983f6bebcc3604b5b31d5fab
slavon0089 <72012086+slavon0089@users.noreply.github.com>
2022-12-29 21:32:23 +0100

    updated feature and path

--- src/test/java/com/epam/tc/hw5/step/DaysOfWeekStep.java
+++ src/test/java/com/epam/tc/hw5/step/DaysOfWeekStep.java
@@ -1,8 +1,28 @@
 package com.epam.tc.hw5.step;
 
-public class DaysOfWeekStep {
-    @Given ("Today is Sunday")
-    public void today_is_sunday(){
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
+public class DaysOfWeekStep {
+    @Given("today is Sunday")
+    public void todayIsSunday() {
+        System.out.println("today is Sunday");
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void iAskWhetherItSFridayYet() {
+        System.out.println("Is it Friday?");
+    }
+
+    @Then("I should be told Nope")
+    public void iShouldBeToldNope() {
+        System.out.println("Nope");
+    }
+
+    @And("Some text")
+    public void some_text() {
+        System.out.println(24);
     }
 }
--- src/test/resources/features/DaysOfWeek.feature
+++ src/test/resources/features/DaysOfWeek.feature
@@ -4,4 +4,5 @@ Feature: Is it Friday yet?
   Scenario: Sunday isn't Friday
     Given today is Sunday
     When  I ask whether it's Friday yet
-    Then I should be told "Nope"
+    And Some text
+    Then I should be told Nope
